06514ad296cbafd50e53d850e4a0f5d00c79700a
jaimezzapata <zapataval2304@gmail.com>
2024-06-15 10:21:18 -0500

    Se implementa método crear/registrar

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+let usuarios = [];
+let usuario = {};
+
+function createUser() {
+  let name = document.querySelector("#r-name").value;
+  let email = document.querySelector("#r-email").value;
+  let password = document.querySelector("#r-password").value;
+  let phone = document.querySelector("#r-phone").value;
+  usuario = {
+    name,
+    email,
+    password,
+    phone,
+  };
+  console.log(usuario);
+  usuarios.push(usuario);
+  console.log(usuarios);
+}
+
+document.getElementById("createUser").addEventListener("click", createUser);
+
 function showFormRegister() {
   document.getElementById("formLogin").style.display = "none";
   document.getElementById("formRegister").style.display = "block";
 }
-
 function showFormLogin() {
   document.getElementById("formLogin").style.display = "block";
   document.getElementById("formRegister").style.display = "none";
 }
-
 document
   .getElementById("showFormLogin")
   .addEventListener("click", showFormLogin);
+document
+  .getElementById("showFormRegister")
+  .addEventListener("click", showFormRegister);
